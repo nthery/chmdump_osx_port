244671a108c25e0ee2972b30a0449539e2ef1d9e
Nicolas Thery <nthery@gmail.com>
2010-12-29 10:41:35 +0100

    fix endianness check
    The check for endianness was incorrectly testing the existence of the
    BIG_ENDIAN macro which is always defined irrespective of the machine
    endianness.  See /usr/include/i386/endian.h on OSX for details.

--- fixendian.h
+++ fixendian.h
@@ -1,4 +1,4 @@
-#ifdef BIG_ENDIAN
+#if BYTE_ORDER == BIG_ENDIAN
 #define EREV32(x) ((((x)&0xFF000000)>>24) | \
                    (((x)&0x00FF0000)>> 8) | \
                    (((x)&0x0000FF00)<< 8) | \
@@ -8,8 +8,8 @@
 #define COPYENDIAN32(x) EREV32((ulong)x)
 #define COPYENDIAN16(x) ((((ushort)(x))>>8) | ((ushort)((x)<<8)))
 #else
-#define FIXENDIAN32(x) 
-#define FIXENDIAN16(x) 
+#define FIXENDIAN32(x)
+#define FIXENDIAN16(x)
 #define COPYENDIAN32(x) x
 #define COPYENDIAN16(x) x
 #endif
